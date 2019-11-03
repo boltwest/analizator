3da82fa5206b9aec5b1b2c307b0c9e95c6e922d5
Igor Gramovich <boltwest@gmail.com>
2019-11-03 12:12:19 +0300

    поправил работу кнопки

--- scetchSensor/scetchSensor.ino
+++ scetchSensor/scetchSensor.ino
@@ -5,15 +5,15 @@
 #define ONE_WIRE_BUS 2
 #define INTERVAL_MIN 3
 #define COUNT_INDICATION 480
-#define MODE_ADDRESS 510
-#define MODE_ANALYZE 1
-#define MODE_ANALYZE_START 2
-#define MODE_ANALYZE_DONE 3
-#define MODE_REPORT 4
 #define LED 13
 #define BUTTON 3
+#define BTN_HOLD_TIME 2000
 
-int MODE;
+boolean buttonActive = false;
+boolean longPressActive = false;
+long buttonTimer = 0;
+
+
 unsigned long periodTime = (long)INTERVAL_MIN * 60 * 1000;
 unsigned long myTimer;
 
@@ -28,83 +28,87 @@ void setup() {
     pinMode(LED, OUTPUT);
     pinMode(BUTTON, INPUT_PULLUP);
     myTimer = millis();
-
-    MODE = EEPROM.read(MODE_ADDRESS);
-
-    initialize();
-
-    switch (MODE) {
-        case MODE_ANALYZE:
-            analyze();
-            break;
-        case MODE_REPORT:
-            report();
-            break;
-    }
 }
 
 void loop() {
     if (!digitalRead(BUTTON)) {
-        Serial.print("MODE: ");
-        Serial.println(MODE);
-        printEeprom();
-        delay(100);
-    }
+
+		if (!buttonActive) {
+			buttonActive = true;
+			buttonTimer = millis();
+		}
+
+		if ((millis() - buttonTimer > BTN_HOLD_TIME) && !longPressActive) {
+			longPressActive = true;
+            // Serial.println("loooong pressss");
+            analyze();
+		}
+
+	} else {
+		if (buttonActive) {
+			if (longPressActive) {
+				longPressActive = false;
+			} else {
+				// Serial.println("short pres");
+                report();
+			}
+			buttonActive = false;
+		}
+	}
 }
 
 void analyze() {
-    EEPROM.write(MODE_ADDRESS, MODE_ANALYZE_START);
-    Serial.println("analyze mode start"); // TODO delet
+    Serial.println("analyze mode start");   // TODO delet
     Serial.println("cler EEPROM starting"); // TODO delet
+    digitalWrite(LED, HIGH);
     clearEeprom();
     Serial.println("cler EEPROM Done"); // TODO delet
+    digitalWrite(LED, LOW);
     sensors.begin();
     int count = 0;
-    while (count <= COUNT_INDICATION) {
+    while (count <= COUNT_INDICATION)
+    {
         if ((long)millis() - myTimer > periodTime) {
+            digitalWrite(LED, HIGH);
             myTimer = millis();
             sensors.requestTemperatures();
             int ter = encode(sensors.getTempCByIndex(0));
             EEPROM.write(count, ter);
             Serial.print("analyze mode \t Temperature: "); // TODO delet
-            Serial.print(decode(ter));     // TODO delet
-            Serial.print("\tcount: "); // TODO delet
-            Serial.println(count); // TODO delet
+            Serial.print(decode(ter));                     // TODO delet
+            Serial.print("\tcount: ");                     // TODO delet
+            Serial.println(count);                         // TODO delet
             count++;
+            digitalWrite(LED, LOW);
         }
     };
-    EEPROM.write(MODE_ADDRESS, MODE_ANALYZE_DONE);
     digitalWrite(LED, HIGH);
     Serial.println("analyze Done"); // TODO delet
 }
 
 void report() {
     Serial.println("report mode start");
+    digitalWrite(LED, HIGH);
     printEeprom();
     Serial.println("report Done");
-    EEPROM.write(MODE_ADDRESS, MODE_ANALYZE);
-}
-
-void initialize() {
-    if (MODE == MODE_ANALYZE_START) {
-        MODE = MODE_REPORT;
-        EEPROM.write(MODE_ADDRESS, MODE_REPORT);
-    }
+    digitalWrite(LED, LOW);
 }
 
 void clearEeprom() {
-    for (int i = 0; i <= COUNT_INDICATION; i++) {
+    for (int i = 0; i <= COUNT_INDICATION; i++)
+    {
         EEPROM.write(i, 0);
     }
 }
 
 void printEeprom() {
-    for (int i = 0; i <= COUNT_INDICATION; i++) {
+    for (int i = 0; i <= COUNT_INDICATION; i++)
+    {
         Serial.println(decode(EEPROM.read(i)));
     }
 }
 
-int encode (float t) {
+int encode(float t) {
     return round((t * 10) - 100);
 }
 
